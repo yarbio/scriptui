f15f94c8a1a157717dc68218c4a2ed71ca441f24
Yarbi <130871160+yarbio@users.noreply.github.com>
2024-06-04 15:50:38 -0500

    the updated one!
    still not done!

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import customtkinter as ctk
+import datetime as dtm
+
+current_date = dtm.datetime.now()
+date1 = (current_date.strftime('%y/%m/%d                               %H:%M'))
 
 win = ctk.CTk()
-win.title('First Gui')
-win.geometry('500x600+550+25')
+win.title('Tasks')
+win.geometry('750x600+550+25')
 
-title1 = ctk.CTkLabel(win, text='Hello, Username', font=('Century Gothic', 40, 'bold'))
-title1.pack(pady = 9)
+title1 = ctk.CTkLabel(win, text='Tasks', font=('Century Gothic', 40, 'bold'))
+title1.pack(pady = 5)
+
+dropdownitems = ['Dahood', 'MM2', 'Universal']
+dropdown1 = ctk.CTkComboBox(win, values=dropdownitems, dropdown_text_color='#BFA0FF', text_color='#BFA0FF', border_color='#BFA0FF', dropdown_font=('Century Gothic', 20, 'bold'), font=('Century Gothic', 20, 'bold'))
+dropdown1.place(x = 30, y = 65)
 
 frame1 = ctk.CTkFrame(win, height=225, width = 300)
-frame1.place(x = 30, y =75)
+frame1.place(x = 30, y =100)
 
 title2 = ctk.CTkLabel(master=frame1, text='Topic 1', font=('Century Gothic', 30, 'bold'))
 title2.place(x = 105, y = 5)
@@ -19,10 +27,16 @@ frame2.place(x = 12, y = 50)
 frame3 = ctk.CTkFrame(master = frame1, height = 150, width = 275)
 frame3.place(x = 12, y = 65)
 
-entry1 = ctk.CTkLabel(master=frame3, text='I AM A TITLE FOR NOW', font=('Century Gothic', 21, 'bold'), width=255)
+entry1 = ctk.CTkEntry(master=frame3, placeholder_text='Title', font=('Century Gothic', 22, 'bold'), width=255)
 entry1.place(x = 10, y = 10)
 
-title3 = ctk.CTkLabel(master= frame3, text='BEST THING EVER AM I RIGHT?', font=('Century Gothic', 14, 'italic'))
-title3.place(x = 50, y = 70)
+title3 = ctk.CTkLabel(master= frame3, text='| Description |', font=('Century Gothic', 22, 'bold'))
+title3.place(x = 50, y = 55)
+
+textbox1 = ctk.CTkTextbox(master= frame3, font=('Century Gothic', 22, 'bold'), width=250,  height=25)
+textbox1.place(x = 10, y = 100)
+
+title4 = ctk.CTkLabel(win, text=date1, font=('Courier', 28, 'bold'))
+title4.place(x = 10, y = 560)
 
 win.mainloop()
